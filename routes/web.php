663b783755ed7c8bca2b3d7b471a30f941499e7a
Khaled Mohammed <mohammed.khaled@uit.ac.ma>
2025-06-25 19:20:09 +0100

    fixe session:table error by re-generate new migration

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@ use App\Http\Controllers\AdminAuthController;
 use Illuminate\Support\Facades\Route;
 
 
-/*Route::middleware(['guest'])->group(function () {
+Route::middleware(['guest'])->group(function () {
   Route::get('/', [AdminAuthController::class, 'showLoginForm'])->name('login');
   Route::post('/', [AdminAuthController::class, 'login']);
-});*/
-Route::get('/login', [AdminAuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AdminAuthController::class, 'login']);
+});
